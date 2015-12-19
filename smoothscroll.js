e53056af67d56c1c1fa43acbf5405a450e95391f
mfrancis95 <mikefrancis95@gmail.com>
2015-12-18 23:51:21 -0500

    Finishes immediately if the start position already equals the end position.

--- smoothscroll.js
+++ smoothscroll.js
@@ -1,29 +1,36 @@
 var smoothScroll = function(properties) {
     properties = properties || {};
     var callback = properties.callback;
-    var duration = properties.duration || 500;
-    var ease = properties.ease;
-    if (!ease) {
-        ease = function(time, start, change, duration) {
-            return change * time / duration + start;
-        };
-    }
     var position = properties.position || 0;
     var start = window.scrollY;
-    var change = position - start;
-    var startTime = Date.now();
-    var step = function() {
-        var time = Date.now() - startTime;
-        if (time <= duration) {
-            window.scrollTo(0, ease(time, start, change, duration));
-            requestAnimationFrame(step);
+    if (position === start) {
+        if (callback) {
+            callback();
         }
-        else {
-            window.scrollTo(0, position);
-            if (callback) {
-                callback();
+    }
+    else {
+        var change = position - start;
+        var duration = properties.duration || 500;
+        var ease = properties.ease;
+        if (!ease) {
+            ease = function (time, start, change, duration) {
+                return change * time / duration + start;
+            };
+        }
+        var startTime = Date.now();
+        var step = function () {
+            var time = Date.now() - startTime;
+            if (time <= duration) {
+                window.scrollTo(0, ease(time, start, change, duration));
+                requestAnimationFrame(step);
             }
-        }
-    };
-    step();
+            else {
+                window.scrollTo(0, position);
+                if (callback) {
+                    callback();
+                }
+            }
+        };
+        step();
+    }
 };
