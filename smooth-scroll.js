79151f390def1216647c10e0d25fb88013e21ef1
Michael Francis <4574480+mfrancis95@users.noreply.github.com>
2023-12-22 22:01:17 -0500

    Modernise script a bit for 2023

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -1,71 +1,20 @@
-var easingFunctions = {
-    linear: function(time, start, change, duration) {
-        return change * time / duration + start;
-    },
-    easeInQuad: function(time, start, change, duration) {
-        return change * (time /= duration) * time + start;
-    },
-    easeOutQuad: function(time, start, change, duration) {
-        return -change * (time /= duration) * (time - 2) + start;
-    },
-    easeInOutQuad: function(time, start, change, duration) {
-        if ((time /= duration / 2) < 1) {
-            return change / 2 * time * time + start;
+const easingFunctions = {
+    easeInCirc: (time, start, change, duration) => -change * (Math.sqrt(1 - (time /= duration) * time) - 1) + start,
+    easeInCubic: (time, start, change, duration) => change * (time /= duration) * time * time + start,
+    easeInExpo: (time, start, change, duration) =>  time === 0 ? start : change * Math.pow(2, 10 * (time / duration - 1)) + start,
+    easeInOutCirc: (time, start, change, duration) => {
+        if ((time /= duration / 2 ) < 1) {
+            return -change / 2 * (Math.sqrt(1 - time * time) - 1) + start;
         }
-        return -change / 2 * ((--time) * (time - 2) - 1) + start;
+        return change / 2 * (Math.sqrt(1 - (time -= 2) * time ) + 1) + start;
     },
-    easeInCubic: function(time, start, change, duration) {
-        return change * (time /= duration) * time * time + start;
-    },
-    easeOutCubic: function(time, start, change, duration) {
-        return change * ((time = time / duration - 1) * time * time + 1) + start;
-    },
-    easeInOutCubic: function(time, start, change, duration) {
+    easeInOutCubic: (time, start, change, duration) => {
         if ((time /= duration / 2) < 1) {
             return change / 2 * time * time * time + start;
         }
         return change / 2 * ((time -= 2) * time * time + 2) + start;
     },
-    easeInQuart: function(time, start, change, duration) {
-        return change * (time /= duration) * time * time * time + start;
-    },
-    easeOutQuart: function(time, start, change, duration) {
-        return -change * ((time = time / duration - 1) * time * time * time - 1) + start;
-    },
-    easeInOutQuart: function(time, start, change, duration) {
-        if ((time /= duration / 2) < 1) {
-            return change / 2 * time * time * time * time + start;
-        }
-        return -change / 2 * ((time -= 2) * time * time * time - 2) + start;
-    },
-    easeInQuint: function(time, start, change, duration) {
-        return change * (time /= duration) * time * time * time * time + start;
-    },
-    easeOutQuint: function(time, start, change, duration) {
-        return change * ((time = time / duration - 1) * time * time * time * time + 1) + start;
-    },
-    easeInOutQuint: function(time, start, change, duration) {
-        if ((time /= duration / 2) < 1) {
-            return change / 2 * time * time * time * time * time + start;
-        }
-        return change / 2 * ((time -= 2) * time * time * time * time + 2) + start;
-    },
-    easeInSine: function(time, start, change, duration) {
-        return -change * Math.cos(time / duration * Math.PI / 2) + change + start;
-    },
-    easeOutSine: function(time, start, change, duration) {
-        return change * Math.sin(time / duration * Math.PI / 2) + start;
-    },
-    easeInOutSine: function(time, start, change, duration) {
-        return -change / 2 * (Math.cos(Math.PI * time / duration) - 1) + start;
-    },
-    easeInExpo: function(time, start, change, duration) {
-        return time === 0 ? start : change * Math.pow(2, 10 * (time / duration - 1)) + start;
-    },
-    easeOutExpo: function(time, start, change, duration) {
-        return time === duration ? start + change : change * (-Math.pow(2, -10 * time / duration) + 1) + start;
-    },
-    easeInOutExpo: function(time, start, change, duration) {
+    easeInOutExpo: (time, start, change, duration) => {
         if (time === 0) {
             return start;
         }
@@ -77,59 +26,60 @@ var easingFunctions = {
         }
         return change / 2 * (-Math.pow(2, -10 * --time) + 2) + start;
     },
-    easeInCirc: function(time, start, change, duration) {
-        return - change * (Math.sqrt(1 - (time /= duration) * time) - 1) + start;
-    },
-    easeOutCirc: function(time, start, change, duration) {
-        return change * Math.sqrt(1 - (time = time / duration - 1) * time) + start;
-    },
-    easeInOutCirc: function(time, start, change, duration) {
-        if ((time /= duration / 2 ) < 1) {
-            return -change / 2 * (Math.sqrt(1 - time * time) - 1) + start;
+    easeInOutQuad: (time, start, change, duration) => {
+        if ((time /= duration / 2) < 1) {
+            return change / 2 * time * time + start;
         }
-        return change / 2 * (Math.sqrt(1 - (time -= 2) * time ) + 1) + start;
-    }
+        return -change / 2 * ((--time) * (time - 2) - 1) + start;
+    },
+    easeInOutQuart: (time, start, change, duration) => {
+        if ((time /= duration / 2) < 1) {
+            return change / 2 * time * time * time * time + start;
+        }
+        return -change / 2 * ((time -= 2) * time * time * time - 2) + start;
+    },
+    easeInOutQuint: (time, start, change, duration) => {
+        if ((time /= duration / 2) < 1) {
+            return change / 2 * time * time * time * time * time + start;
+        }
+        return change / 2 * ((time -= 2) * time * time * time * time + 2) + start;
+    },
+    easeInOutSine: (time, start, change, duration) => -change / 2 * (Math.cos(Math.PI * time / duration) - 1) + start,
+    easeInQuad: (time, start, change, duration) => change * (time /= duration) * time + start,
+    easeInQuart: (time, start, change, duration) => change * (time /= duration) * time * time * time + start,
+    easeInQuint: (time, start, change, duration) =>  change * (time /= duration) * time * time * time * time + start,
+    easeInSine: (time, start, change, duration) => -change * Math.cos(time / duration * Math.PI / 2) + change + start,
+    easeOutCirc: (time, start, change, duration) => change * Math.sqrt(1 - (time = time / duration - 1) * time) + start,
+    easeOutCubic: (time, start, change, duration) => change * ((time = time / duration - 1) * time * time + 1) + start,
+    easeOutExpo: (time, start, change, duration) => time === duration ? start + change : change * (-Math.pow(2, -10 * time / duration) + 1) + start,
+    easeOutQuad: (time, start, change, duration) => -change * (time /= duration) * (time - 2) + start,
+    easeOutQuart: (time, start, change, duration) => -change * ((time = time / duration - 1) * time * time * time - 1) + start,
+    easeOutQuint: (time, start, change, duration) => change * ((time = time / duration - 1) * time * time * time * time + 1) + start,
+    easeOutSine: (time, start, change, duration) => change * Math.sin(time / duration * Math.PI / 2) + start,
+    linear: (time, start, change, duration) => change * time / duration + start
 };
 
-function smoothScroll(position, duration, ease, interruptible, callback) {
-    if (!position) {
-        position = 0;
-    }
-    var start = window.scrollY;
+async function smoothScroll(position = 0, duration = 500, ease = easingFunctions.linear, interruptible = []) {
+    const start = window.scrollY;
     if (position === start) {
-        if (callback) {
-            callback();
-        }
+        return;
     }
-    else {
-        if (duration !== 0 && !duration) {
-            duration = 500;
-        }
-        switch (typeof ease) {
-            case "function":
-                break;
-            case "string":
-                ease = easingFunctions[ease] || easingFunctions.linear;
-                break;
-            default:
-                ease = easingFunctions.linear;
-        }
-        var run = true;
-        if (interruptible && interruptible.length > 0) {
-            var interrupt = function() {
-                run = false;
-            };
-            for (var i = 0; i < interruptible.length; i++) {
-                document.addEventListener(interruptible[i], interrupt);
-            }
-        }
-        var startTime = Date.now();
-        var change = position - start;
-        var step = function() {
+    const easingFunction = typeof ease === 'string' ? easingFunctions[ease] ?? easingFunctions.linear : ease;
+    let run = true;
+    const interrupt = () => {
+        run = false;
+    };
+    interruptible.forEach(event => {
+        document.addEventListener(event, interrupt)
+    });
+    const startTime = Date.now();
+    const change = position - start;
+    return new Promise(resolve => {
+        const step = () => {
             if (run) {
-                var time = Date.now() - startTime;
+                const time = Date.now() - startTime;
                 if (time <= duration) {
-                    window.scrollTo(0, ease(time, start, change, duration));
+                    window.scrollTo(0, easingFunction(time, start, change, duration));
                 }
                 else {
                     run = false;
@@ -138,60 +88,29 @@ function smoothScroll(position, duration, ease, interruptible, callback) {
                 requestAnimationFrame(step);
             }
             else {
-                if (interruptible) {
-                    for (var i = 0; i < interruptible.length; i++) {
-                        document.removeEventListener(interruptible[i], interrupt);
-                    }
-                }
-                if (callback) {
-                    callback();
-                }
+                interruptible.forEach(event => {
+                    document.removeEventListener(event, interrupt);
+                });
+                resolve();
             }
         };
-        step();
-    }
+        requestAnimationFrame(step);
+    });
 }
 
-Element.prototype.smoothScrollify = function($scope, attrs) {
-    var self = this;
-    if (!attrs) {
-        attrs = self.dataset;
-    }
-    self.addEventListener("click", function(event) {
+HTMLElement.prototype.smoothScrollify = function() {
+    const { dataset } = this;
+    this.addEventListener('click', async event => {
         event.preventDefault();
-        var href= self.getAttribute("href");
-        if (attrs.history) {
-            history.pushState(null, null, href);
+        const href = this.getAttribute('href');
+        if (dataset.history) {
+            history.pushState(null, '', href);
         }
-        var position = document.getElementById(href.slice(1)).offsetTop;
-        if (attrs.interruptible) {
-            var interruptible = attrs.interruptible.split(" ");
-        }
-        if (attrs.callback) {
-            if ($scope) {
-                var callback = function() {
-                    $scope[attrs.callback](self);
-                };
-            }
-            else {
-                var callback = function() {
-                    window[dataset.callback](self);
-                };
-            }
-        }
-        smoothScroll(position, attrs.duration, attrs.ease, interruptible, callback);
+        await smoothScroll(document.getElementById(href.slice(1)).offsetTop, dataset.duration, dataset.ease, dataset.interruptible?.split(' '));
+        window[dataset.callback]?.();
     });
 };
 
-if (angular) {
-    angular.module("smooth-scroll", []).directive("smoothScroll", function() {
-        return function($scope, element, attrs) {
-            element[0].smoothScrollify($scope, attrs);
-        }
-    });
-}
-
-var elements = document.getElementsByClassName("smooth-scroll");
-for (var i = 0; i < elements.length; i++) {
-    elements[i].smoothScrollify();
+for (const element of document.getElementsByClassName('smooth-scroll')) {
+    element.smoothScrollify();
 }
